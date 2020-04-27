a339464b538770a2ef9c5b66bbdf36ba52e7ce1b
Jungwon Oh <jungwon.oh87@gmail.com>
2020-04-26 20:05:59 -0700

    api fetched with key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import axios from "axios";
 import { Nav } from "./components/nav/nav.component";
+import { NowPlaying } from "./components/now-playing/now.component";
 
 import "./App.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
   constructor() {
@@ -10,22 +13,25 @@ class App extends React.Component {
   }
 
   state = {
-    persons: [],
+    movies: [],
   };
 
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      console.log(res);
-      this.setState({ persons: res.data }, () => {
-        console.log(this.state.persons);
+    axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`)
+      .then((res) => {
+        // console.log(res);
+        this.setState({ movies: res.data }, () => {
+          console.log(this.state.movies);
+        });
       });
-    });
   }
 
   render() {
     return (
       <div className="App">
         <Nav></Nav>
+        <NowPlaying></NowPlaying>
       </div>
     );
   }
