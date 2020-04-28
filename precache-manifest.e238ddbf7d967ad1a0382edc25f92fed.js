5e14e5f9d670c0007ae2adb88bbce34f5314f713
Jungwon Oh <jungwon.oh87@gmail.com>
2020-04-28 10:17:16 -0700

    Updates

---
+++ precache-manifest.e238ddbf7d967ad1a0382edc25f92fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d1b7b1b5d8107c1387b420dd5e2313",
+    "url": "/netflix_clone/index.html"
+  },
+  {
+    "revision": "5976e3c05af752f90359",
+    "url": "/netflix_clone/static/css/main.69491c45.chunk.css"
+  },
+  {
+    "revision": "09ae0c0b6bbb7b3c99bd",
+    "url": "/netflix_clone/static/js/2.fa9b6eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix_clone/static/js/2.fa9b6eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5976e3c05af752f90359",
+    "url": "/netflix_clone/static/js/main.571d78b5.chunk.js"
+  },
+  {
+    "revision": "0a712c896519ee943e70",
+    "url": "/netflix_clone/static/js/runtime-main.b84e150f.js"
+  }
+]);
